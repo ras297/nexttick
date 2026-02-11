202495e318c37073e26b8cfcaf8363477fd4f751
Rasool Malekinejad <rasool.malekinejad@gmail.com>
2026-02-11 20:14:24 +0330

    Add TimeSlot Entity (DDD root Aggregate)

---
+++ domain/src/main/java/ir/maleki/sideprojects/nexttick/domain/TimeSlot.java
@@ -0,0 +1,55 @@
+package ir.maleki.sideprojects.nexttick.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "AVAILABLE_SLOTS")
+public class TimeSlot extends BaseEntity {
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    @Column(name = "IS_RESERVED")
+    private Boolean reserved;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "holder_id")
+    private User holder;
+
+    public LocalDateTime startTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime endTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public Boolean isReserved() {
+        return reserved;
+    }
+
+    public void setReserved(Boolean reserved) {
+        this.reserved = reserved;
+    }
+
+    public User holder() {
+        return holder;
+    }
+
+    public void setHolder(User holder) {
+        this.holder = holder;
+    }
+}
