620b4424a1cbcbda96ecffd947b5aa2bcd514686
Rasool Malekinejad <rasool.malekinejad@gmail.com>
2026-02-11 07:43:04 +0330

    Bootstrap NextTick Spring Boot application

---
+++ src/main/java/ir/maleki/sideprojects/nexttick/NexttickApplication.java
@@ -0,0 +1,13 @@
+package ir.maleki.sideprojects.nexttick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexttickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexttickApplication.class, args);
+	}
+
+}
