df9b98df40e15e21a331db00d4dbe77614159f3d
Rasool Malekinejad <rasool.malekinejad@gmail.com>
2026-02-12 05:24:57 +0330

    Fix invalid @NotBlank usage on Long field userId

--- application/src/main/java/ir/maleki/sideprojects/nexttick/application/reservation/ReserveTimeSlot.java
+++ application/src/main/java/ir/maleki/sideprojects/nexttick/application/reservation/ReserveTimeSlot.java
@@ -1,8 +1,8 @@
 package ir.maleki.sideprojects.nexttick.application.reservation;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.io.Serializable;
 
-public record ReserveTimeSlot(@NotBlank Long userId) implements Serializable {
+public record ReserveTimeSlot(@NotNull Long userId) implements Serializable {
 }
