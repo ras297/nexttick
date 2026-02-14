294ab4cb7c3f79189b9dbb7ea88df52de0cec6d2
Rasool Malekinejad <rasool.malekinejad@gmail.com>
2026-02-14 09:47:03 +0330

    Add log of queue initialization

--- application/src/main/java/ir/maleki/sideprojects/nexttick/application/reservation/QueueInitializer.java
+++ application/src/main/java/ir/maleki/sideprojects/nexttick/application/reservation/QueueInitializer.java
@@ -4,6 +4,7 @@ import ir.maleki.sideprojects.nexttick.domain.service.TimeSlotInfo;
 import ir.maleki.sideprojects.nexttick.domain.service.TimeSlotPriority;
 import ir.maleki.sideprojects.nexttick.domain.service.TimeSlotPriorityQueue;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,7 @@ import java.time.ZoneOffset;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @AllArgsConstructor
 @Component
 public class QueueInitializer {
@@ -21,6 +23,9 @@ public class QueueInitializer {
 
     @EventListener(ApplicationReadyEvent.class)
     public void initializeQueue() {
+        log.info("Starting TimeSlotPriorityQueue rebuild...");
+        long start = System.currentTimeMillis();
+
         List<TimeSlotPriority> items = repository.findAllByReservedFalseAndStartTimeAfter(LocalDateTime.now())
                 .stream()
                 .map(slot -> new TimeSlotPriority(
@@ -30,5 +35,10 @@ public class QueueInitializer {
                 .collect(Collectors.toList());
 
         timeSlotPriorityQueue.rebuild(items);
+
+        long duration = System.currentTimeMillis() - start;
+
+        log.info("Queue rebuild completed: {} slots loaded in {} ms",
+                items.size(), duration);
     }
 }
